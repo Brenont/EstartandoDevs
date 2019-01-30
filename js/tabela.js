82876ee3bfc5b5ad2234ed202b859c3fe6a7ccde
Breno Nunes <Brenontavares@gmail.com>
2019-01-30 10:43:44 -0200

    Processo seletivo 2019

--- js/tabela.js
+++ js/tabela.js
@@ -1,36 +1,36 @@
-var lista = ["maria Luiza Soares da Silva",
-    "karolina Andrade de Freitas",
-    "mariana Lustosa de Queiroz",
-    "igor Daniel da silva",
-    "thiago lucio",
-    "karoline rufino da silva felismino",
-    "juliana lima vieira",
-    "ramon ferreira",
-    "vitoria avoglio macedo",
-    "john wesley ribeiro costa",
-    "ana beatriz bertine da s. de oliveira",
-    "yuri da silva dos santos",
-    "kaua mateus de goes",
-    "jessica dos santos da silva",
-    "lenita oleveira",
-    "yago araujo valuche pina",
-    "alisson braz silva da costa",
+// var lista = ["maria Luiza Soares da Silva",
+//     "karolina Andrade de Freitas",
+//     "mariana Lustosa de Queiroz",
+//     "igor Daniel da silva",
+//     "thiago lucio",
+//     "karoline rufino da silva felismino",
+//     "juliana lima vieira",
+//     "ramon ferreira",
+//     "vitoria avoglio macedo",
+//     "john wesley ribeiro costa",
+//     "ana beatriz bertine da s. de oliveira",
+//     "yuri da silva dos santos",
+//     "kaua mateus de goes",
+//     "jessica dos santos da silva",
+//     "lenita oleveira",
+//     "yago araujo valuche pina",
+//     "alisson braz silva da costa",
 
-];
-var listaHtml = "";
-lista = lista.sort();
-var i = 1
+// ];
+// var listaHtml = "";
+// lista = lista.sort();
+// var i = 1
 
-$(document).ready(function() {
-    lista.forEach(item => {
-        listaHtml += "<tr>" +
-            "<td>" + i + " - " + item.toUpperCase() + "</td>" +
-            "</tr>";
-        i++;
+// $(document).ready(function() {
+//     lista.forEach(item => {
+//         listaHtml += "<tr>" +
+//             "<td>" + i + " - " + item.toUpperCase() + "</td>" +
+//             "</tr>";
+//         i++;
 
-    });
-    document.getElementById("tblLinha").innerHTML += listaHtml;
-});
+//     });
+//     document.getElementById("tblLinha").innerHTML += listaHtml;
+// });
 
 
 // <tr>
